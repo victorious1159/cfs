cdcb076e90422c7e4404c441b31121f0359957a2
pngochuy <pngochuy3010@gmail.com>
2023-09-30 22:34:42 +0700

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -2,18 +2,47 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/MenuComponent.js";
+import Hello from "./components/Hello.js";
+import { DISHES } from "./shared/dishes.js";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu></Menu>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar dark color="primary">
+//         <div className="container">
+//           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+//         </div>
+//       </Navbar>
+//       <Hello myId={"DE170429"} myAge="23" a={10} b={5} />
+//       <Menu dishes={DISHES} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,7 +1,72 @@
-import React from "react";
+// import React, { Component } from "react";
 
-function Hello() {
-  return <>Hello my friend!</>;
+// export class Hello extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0,
+//     };
+//     this.increasingCount = this.increasingCount.bind(this);
+//     this.decreasingCount = this.decreasingCount.bind(this);
+//   }
+
+//   increasingCount() {
+//     this.setState({ count: this.state.count + 1 });
+//   }
+//   decreasingCount() {
+//     this.setState({ count: this.state.count - 1 });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h4>Count: {this.state.count}</h4>
+//         <button onClick={this.increasingCount}>Tăng</button> |
+//         <button onClick={this.decreasingCount}>Giảm</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Hello;
+
+import React, { useState } from "react";
+
+function Hello({ myId, myAge, a, b }) {
+  const [count, setCount] = useState(0);
+
+  const increasingCount = () => {
+    setCount(count + 1);
+  };
+
+  const decreasingCount = () => {
+    setCount(count - 1);
+  };
+
+  const callMe = () => {
+    return "Pham Ngoc Huy";
+  };
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={() => increasingCount()}>Tăng</button>
+      <button onClick={decreasingCount}>Giảm</button>
+
+      {/* gọi hàm ngay lập tức khi render, điều này ko tốt khi sử dụng vs click */}
+      <h2>{callMe()}</h2>
+
+      {/* truyền biến về đây. Nhớ ghi 'myId, myAge, ...' trong {...} trên function 
+      hoặc 'props' trong {...} trên function
+      */}
+      <h2>
+        Id: {myId} - Age: {myAge}
+      </h2>
+
+      <h2>
+        a: {a} - b: {b}
+      </h2>
+    </>
+  );
 }
 
 export default Hello;
