098449e68356ef88cef5d76f67875e43f02d4c47
pngochuy <pngochuy3010@gmail.com>
2023-09-30 15:49:23 +0700

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent.js";
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <div className="App">
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu></Menu>
     </div>
   );
 }
---
+++ src/components/Hello.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Hello() {
+  return <>Hello my friend!</>;
+}
+
+export default Hello;
