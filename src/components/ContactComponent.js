b4b33bb9c7ebb9918670c7d9fe1e244295ead73b
pngochuy <pngochuy3010@gmail.com>
2023-10-21 10:44:47 +0700

    React Redux Form

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -14,6 +14,8 @@ import {
 
 import { Link } from "react-router-dom";
 
+import { Control, LocalForm, Errors } from "react-redux-form";
+
 class Contact extends Component {
   constructor(props) {
     super(props);
@@ -85,12 +87,16 @@ class Contact extends Component {
     });
   }
 
-  handleSubmit(event) {
-    console.log("Current State is: " + JSON.stringify(this.state));
-    alert("Current State is: " + JSON.stringify(this.state));
-    event.preventDefault();
+  handleSubmit(values) {
+    console.log("Current State is: " + JSON.stringify(values));
+    alert("Current State is: " + JSON.stringify(values));
+    // event.preventDefault();
   }
-
+  // handleSubmit(event) {
+  //   console.log("Current State is: " + JSON.stringify(this.state));
+  //   alert("Current State is: " + JSON.stringify(this.state));
+  //   event.preventDefault();
+  // }
   render() {
     const errors = this.validate(
       this.state.firstname,
@@ -116,7 +122,111 @@ class Contact extends Component {
                   <h3>Send us your Feedback</h3>
                 </div>
                 <div className="col-12 col-md-9">
-                  <Form onSubmit={this.handleSubmit}>
+                  <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                    <Row className="form-group">
+                      <Label htmlFor="firstname" md={2}>
+                        First Name
+                      </Label>
+                      <Col md={10}>
+                        <Control.text
+                          model=".firstname"
+                          id="firstname"
+                          name="firstname"
+                          placeholder="First Name"
+                          className="form-control"
+                        />
+                      </Col>
+                    </Row>
+                    <Row className="form-group">
+                      <Label htmlFor="lastname" md={2}>
+                        Last Name
+                      </Label>
+                      <Col md={10}>
+                        <Control.text
+                          model=".lastname"
+                          id="lastname"
+                          name="lastname"
+                          placeholder="Last Name"
+                          className="form-control"
+                        />
+                      </Col>
+                    </Row>
+                    <Row className="form-group">
+                      <Label htmlFor="telnum" md={2}>
+                        Contact Tel.
+                      </Label>
+                      <Col md={10}>
+                        <Control.text
+                          model=".telnum"
+                          id="telnum"
+                          name="telnum"
+                          placeholder="Tel. Number"
+                          className="form-control"
+                        />
+                      </Col>
+                    </Row>
+                    <Row className="form-group">
+                      <Label htmlFor="email" md={2}>
+                        Email
+                      </Label>
+                      <Col md={10}>
+                        <Control.text
+                          model=".email"
+                          id="email"
+                          name="email"
+                          placeholder="Email"
+                          className="form-control"
+                        />
+                      </Col>
+                    </Row>
+                    <Row className="form-group">
+                      <Col md={{ size: 6, offset: 2 }}>
+                        <div className="form-check">
+                          <Label check>
+                            <Control.checkbox
+                              model=".agree"
+                              name="agree"
+                              className="form-check-input"
+                            />{" "}
+                            <strong>May we contact you?</strong>
+                          </Label>
+                        </div>
+                      </Col>
+                      <Col md={{ size: 3, offset: 1 }}>
+                        <Control.select
+                          model=".contactType"
+                          name="contactType"
+                          className="form-control"
+                        >
+                          <option>Tel.</option>
+                          <option>Email</option>
+                        </Control.select>
+                      </Col>
+                    </Row>
+                    <Row className="form-group">
+                      <Label htmlFor="message" md={2}>
+                        Your Feedback
+                      </Label>
+                      <Col md={10}>
+                        <Control.textarea
+                          model=".message"
+                          id="message"
+                          name="message"
+                          rows="12"
+                          className="form-control"
+                        />
+                      </Col>
+                    </Row>
+                    <Row className="form-group">
+                      <Col md={{ size: 10, offset: 2 }}>
+                        <Button type="submit" color="primary">
+                          Send Feedback
+                        </Button>
+                      </Col>
+                    </Row>
+                  </LocalForm>
+
+                  {/* <Form onSubmit={this.handleSubmit}>
                     <FormGroup row>
                       <Label htmlFor="firstname" md={2}>
                         First Name
@@ -242,7 +352,7 @@ class Contact extends Component {
                         </Button>
                       </Col>
                     </FormGroup>
-                  </Form>
+                  </Form> */}
                 </div>
               </div>
             </div>
@@ -301,69 +411,3 @@ class Contact extends Component {
 }
 
 export default Contact;
-
-// function Contact(props) {
-//   return (
-//     <div className="container">
-//       <div className="row">
-//         <Breadcrumb>
-//           <BreadcrumbItem>
-//             <Link to="/home">Home</Link>
-//           </BreadcrumbItem>
-//           <BreadcrumbItem active>Contact Us</BreadcrumbItem>
-//         </Breadcrumb>
-//         <div className="col-12">
-//           <h3>Contact Us</h3>
-//           <hr />
-//         </div>
-//       </div>
-
-//       <div className="row row-content">
-//         <div className="col-12">
-//           <h3>Location Information</h3>
-//         </div>
-//         <div className="col-12 col-sm-4 offset-sm-1">
-//           <h5>Our Address</h5>
-//           <address>
-//             121, Clear Water Bay Road
-//             <br />
-//             Clear Water Bay, Kowloon
-//             <br />
-//             HONG KONG
-//             <br />
-//             <i className="fa fa-phone"></i>: +852 1234 5678
-//             <br />
-//             <i className="fa fa-fax"></i>: +852 8765 4321
-//             <br />
-//             <i className="fa fa-envelope"></i>:{" "}
-//             <a href="mailto:confusion@food.net">confusion@food.net</a>
-//           </address>
-//         </div>
-//         <div className="col-12 col-sm-6 offset-sm-1">
-//           <h5>Map of our Location</h5>
-//         </div>
-//         <div className="col-12 col-sm-11 offset-sm-1">
-//           <div className="btn-group" role="group">
-//             <a
-//               role="button"
-//               className="btn btn-primary"
-//               href="tel:+85212345678"
-//             >
-//               <i className="fa fa-phone"></i> Call
-//             </a>
-//             <a role="button" className="btn btn-info">
-//               <i className="fa fa-skype"></i> Skype
-//             </a>
-//             <a
-//               role="button"
-//               className="btn btn-success"
-//               href="mailto:confusion@food.net"
-//             >
-//               <i className="fa fa-envelope-o"></i> Email
-//             </a>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
